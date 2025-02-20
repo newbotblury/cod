ffcef1287282ebf34a671b1555c3ecf584e4d901
newbotblury <awadheshkumar00083@gmail.com>
2025-02-20 11:57:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @BLURYF4CE
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7239021384:AAH2nNKrkZu8VLwxjBdMUjYC4kyJpYYIOvE")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@spec890i")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7221087191" ,"5736732360")
+
+
+
+
+
+
+
+  
