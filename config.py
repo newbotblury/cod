55285d25d1a66ce6622699730c3403b0fbcfe511
newbotblury <awadheshkumar00083@gmail.com>
2025-04-17 14:47:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 #SCRIPT BY @BLURYF4CE
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7239021384:AAH2nNKrkZu8VLwxjBdMUjYC4kyJpYYIOvE")
+BOT_TOKEN=("7657281742:AAHM9cujthUZ19Nnd_fV3hm3FwcIG42wvKM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@spec890i")
+OWNER_USERNAME=("@sp5000x")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("7221087191" ,"5736732360")
